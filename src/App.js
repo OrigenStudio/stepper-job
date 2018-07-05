7d74293d9c051b36fb03d58d7524a70e3ea9d73e
PolGuixe <pol.guixe@gmail.com>
2018-07-05 10:39:59 +0200

    improved fixtures

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Stepper from "./components/Stepper";
 
 const generateItems = (itemsNumber, completed) => {
   const items = [];
-  let currentStepAssigned = false;
+  let currentItemAssigned = false;
   for (let i = 0; i < itemsNumber; i++) {
     items.push({
       label: `Lorem ipsum ${i}`,
@@ -14,30 +14,40 @@ const generateItems = (itemsNumber, completed) => {
       },
       completed: (i + 1) / itemsNumber < 0.5 || completed ? true : false,
       currentItem:
-        (i + 1) / itemsNumber >= 0.5 && !completed && !currentStepAssigned
+        (i + 1) / itemsNumber >= 0.5 && !completed && !currentItemAssigned
           ? true
           : false
     });
     if ((i + 1) / itemsNumber >= 0.5 && !completed) {
-      currentStepAssigned = true;
+      currentItemAssigned = true;
     }
   }
 
   return items;
 };
 
-const generateStep = completed => {
+const generateStep = (completed, currentStep) => {
   return {
     title: "Lorem ipsum",
     items: generateItems(Math.floor(Math.random() * 5 + 1), completed),
-    completed: completed
+    completed: completed,
+    currentStep: currentStep
   };
 };
 
 const generateSteps = stepsNumber => {
   const steps = [];
+  let currentStepAssigned = false;
   for (let i = 0; i < stepsNumber; i++) {
-    steps.push(generateStep((i + 1) / stepsNumber < 0.5 ? true : false));
+    steps.push(
+      generateStep(
+        (i + 1) / stepsNumber < 0.5 ? true : false,
+        (i + 1) / stepsNumber > 0.5 && !currentStepAssigned
+      )
+    );
+    if ((i + 1) / stepsNumber > 0.5) {
+      currentStepAssigned = true;
+    }
   }
   return steps;
 };
