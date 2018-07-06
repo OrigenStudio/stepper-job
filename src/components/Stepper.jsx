d35414ed4fbecee8a5bae1a3182d42bfffd1a5d1
PolGuixe <pol.guixe@gmail.com>
2018-07-06 19:59:03 +0200

    added expand & collapse control to the stepper

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -4,6 +4,7 @@ import Dimensions from "react-dimensions";
 import classNames from "classnames";
 
 import Bubble from "./Bubble";
+import BubbleContentList from "./BubbleContentList";
 import CollapsibleBubble from "./CollapsibleBubble";
 
 class Stepper extends React.Component {
@@ -22,6 +23,12 @@ class Stepper extends React.Component {
     });
   };
 
+  toggleExpandClick = () => {
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  };
+
   render() {
     const {
       steps,
@@ -34,39 +41,55 @@ class Stepper extends React.Component {
     // TODO remove comment
     console.log(steps);
 
+    const currentStep = steps[findIndex(steps, "currentStep")];
     const verticalFormat = containerWidth <= 768;
     return (
       <div className="stepperWrapper">
-        <div
-          className={classNames("textSection", {
-            textSectionVertical: verticalFormat
-          })}
-        >
-          <span className="title">{title}</span>
-          <span className="subtitle">{subtitle}</span>
-        </div>
-        <div
-          className={classNames({
-            vertical: verticalFormat,
-            horizontal: !verticalFormat
-          })}
-        >
-          <Bubble title={heading} state="heading">
-            <span className="description"> {headingDescription} </span>
+        {this.state.expanded || !verticalFormat ? (
+          <div>
+            <div
+              className={classNames("textSection", {
+                textSectionVertical: verticalFormat
+              })}
+            >
+              <span className="title">{title}</span>
+              <span className="subtitle">{subtitle}</span>
+            </div>
+            <div
+              className={classNames({
+                vertical: verticalFormat,
+                horizontal: !verticalFormat
+              })}
+            >
+              <Bubble title={heading} state="heading">
+                <span className="description"> {headingDescription} </span>
+              </Bubble>
+              {steps.map((step, index) => (
+                <CollapsibleBubble
+                  key={step.title + index}
+                  verticalFormat={verticalFormat}
+                  index={index}
+                  {...step}
+                  first={index === 0}
+                  last={index + 1 === steps.length}
+                  expanded={index === this.state.openStep}
+                  onStepClick={this.handleStepClick}
+                />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <Bubble title={currentStep.title}>
+            <BubbleContentList items={currentStep.items} />
           </Bubble>
-          {steps.map((step, index) => (
-            <CollapsibleBubble
-              key={step.title + index}
-              verticalFormat={verticalFormat}
-              index={index}
-              {...step}
-              first={index === 0}
-              last={index + 1 === steps.length}
-              expanded={index === this.state.openStep}
-              onStepClick={this.handleStepClick}
-            />
-          ))}
-        </div>
+        )}
+        {verticalFormat ? (
+          <div className="expandCollapseWrapper">
+            <span onClick={this.toggleExpandClick}>
+              {this.state.expanded ? "Collapse" : "Expand progress view"}
+            </span>
+          </div>
+        ) : null}
       </div>
     );
   }
